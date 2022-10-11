6737df1372a5f08ded80adf2f18e362a4e3f3d5f
Girish kumar G <girishgopinath190@gmail.com>
2022-10-11 12:37:33 +0530

    store and cart functionality updated

--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.db import models
 from category.models import Category
 from django.urls import reverse
@@ -14,6 +15,8 @@ class Product(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
     
+    class Meta:
+        ordering = ['-id']
 
     def get_url(self):
         return reverse('product_detail', args=[self.category.slug, self.slug])
@@ -21,4 +24,30 @@ class Product(models.Model):
     def __str__(self):
         return self.product_name
 
+
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager, self).filter(variation_category='color', is_active=True)
+
+    def sizes(self):
+        return super(VariationManager, self).filter(variation_category='size', is_active=True)
+
+
+variation_category_choice = (
+    ('color', 'color'),
+    ('size', 'size'),
+)
+
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=variation_category_choice)
+    variation_value =  models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now=True)
+
+    objects = VariationManager()
+
+    def __str__(self):
+        return self.variation_value
     
